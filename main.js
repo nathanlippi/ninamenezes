ca3307e8a3e3ae5ebd35359bec478ac2fceeb584
Nathan Lippi <nathan.lippi@gmail.com>
2014-02-03 12:32:39 -0500

    Initial commit.

---
+++ main.js
@@ -0,0 +1,8 @@
+// function toggle_audio() {
+//     var id = "#audio_player";
+//     var o  = "opacity";
+
+//     $(id).animate({
+//        opacity: $(id).css(o) == 1 ? 0 : 1
+//     }, 1500, function() { });
+// }
